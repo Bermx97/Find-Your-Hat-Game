df2551ade7a89086fb567f009f2cefd4a0dddaaa
Bermx97 <bermxtf@gmail.com>
2025-04-01 22:02:53 +0200

    try add pathPocition method

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const pathCharacter = '*';
 class Field { 
     constructor(field) {
       this._field = field;
+      this._playerRow = 0;
+      this._playerColumn = 0;
     }
     printField () {
       let res = '';
@@ -15,35 +17,68 @@ class Field {
         res += row.join('');
         res += ('\n');
       } console.log(res);
-    }
+    } 
     playerPosition () {
-      let rowField = 0;
-      let columnField = 0;
+      let rowField = []
+      let columnField = []
       for (let row of this._field) {
-        if (row.indexOf('*') > -1) {
-          columnField = row.indexOf('*');
-          break;
-        } else {
-          rowField += 1;
+        let iInRow = 0;
+        for (let symbol of row) {
+          let inColumnField = [];
+          let i = 0
+          if (symbol === '*') {
+            //console.log(symbol)
+            inColumnField.push(i)
+            i++
+          } else {
+            i++
+          } console.log(inColumnField)
         }
-      } return [rowField, columnField];
-    }
+    } console.log(columnField)
+  }
 };
-  
-  const myField = new Field([
-    ['*', '░', 'O', 'O', '░', 'O', '░', 'O', '░',],
+const myField = new Field([
+    ['*', '░', 'O', 'O', '*', 'O', '░', 'O', '░',],
     ['░', 'O', '░', '░', '░', '░', 'O', '░', '░',],
-    ['░', '░', '░', 'O', 'O', '░', '░', '░', 'O',],
-    ['░', 'O', '░', '░', 'O', 'O', 'O', 'O', '░',],
-    ['░', 'O', '░', 'O', 'O', '░', '░', '^', '░',],
-  ]);
+    ['*', '░', '░', 'O', 'O', '░', '░', '░', 'O',],
+    ['*', 'O', '░', '░', 'O', 'O', 'O', 'O', '░',],
+    ['░', 'O', '░', 'O', 'O', '*', '░', '^', '*',],
+]);
   
 
 
 
 
- 
 //node main.js
 myField.printField();
-console.log(myField.playerPosition())
+myField.playerPosition()
+//console.log(myField.playerPosition())
+//console.log(myField._playerRow)
+//console.log(myField._playerColumn)
 
+
+
+
+
+
+
+
+
+
+
+
+
+/* playerPosition () {
+  let rowField = 0;
+  let columnField = 0;
+  for (let row of this._field) {
+    if (row.indexOf('*') > -1) {
+      columnField = row.indexOf('*');
+      break;
+    } else {
+      rowField += 1;
+    }
+  } return [rowField, columnField];
+}
+};
+*/
